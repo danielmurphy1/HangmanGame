c624adb844de5b6c5e9276299d445149b0809181
danielmurphy1 <daniel.murphy.1@gmail.com>
2023-08-22 23:47:12 -0400

    Add project files.

---
+++ HangmanGame/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+string wordList = File.ReadAllText("../../../../word_list.txt");
+string[] words = wordList.Split(",");
+
+
