846464150a5473c6e35c5cc4d92b78267da5fed1
danielmurphy1 <daniel.murphy.1@gmail.com>
2023-08-23 21:16:51 -0400

    Added GameScreen Class and generated welcome message for beginning of app

---
+++ HangmanGame/GameScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGame
+{
+    public class GameScreen
+    {
+        public void GenerateWelcomeMessage()
+        {
+            Console.Clear();
+            Console.WriteLine("\n\n\n\n\n\n");
+            Console.WriteLine("\t\t\t\t\t\tWelcome to Console Hangman!");
+            Console.WriteLine("\n\n\t\t\t\t\tGuess the correct word before you are hung.");
+            Console.WriteLine("\t\t\tAfter a correct letter guessed, that letter will take its place in the mystery word.");
+            Console.WriteLine("\t\tAfter each incorrect guess, an additional piece of the gallows or of the hangman will be drawn.");
+            Console.WriteLine("\t\t\tIf all pieces are drawn before you guess the mystery word correctly, you lose.");
+            Console.WriteLine("\t\t\t\t\tYou have a total of seven guesses. Good Luck!");
+            Console.WriteLine("\t\t\t\t\t\tPress SPACEBAR to play.");
+            Console.WriteLine("\t\t\t\t\t\tPress Q to quit.");
+        }
+    }
+}
--- HangmanGame/Program.cs
+++ HangmanGame/Program.cs
@@ -1,7 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+using HangmanGame;
 
 string wordList = File.ReadAllText("../../../../word_list.txt");
 string[] words = wordList.Split(",");
 
+GameScreen gameScreen = new GameScreen();
 
+gameScreen.GenerateWelcomeMessage();
+
+if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+{
+    Console.WriteLine("Temp Holder - Space Pressed");
+}
+else if (Console.ReadKey().Key == ConsoleKey.Q ) //takes two presses - why?
+{
+    Console.WriteLine("Temp Holder - Q Pressed");
+}
+else
+{
+    //add another key pressed scenario
+
+}
